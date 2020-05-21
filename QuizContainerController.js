3c557241dbcb0209c99bc49701b755f7c9bfff81
Seth Ortiz <sethl.ortiz@gmail.com>
2020-05-21 10:15:36 -0700

    Create QuizContainerController.js

---
+++ QuizContainerController.js
@@ -0,0 +1,26 @@
+({
+    handleQuizStageChange : function(component, event,helper) {
+        let stage = event.getParam("stage");
+        let quizId = event.getParam("quizId");
+        let attempt = event.getParam("attmept");
+        let results = event.getParam("results");
+        switch(stage) {
+            case "QUIZ":
+                component.set("v.registerOpen", false);
+                component.set("v.quizOpen", true);
+                component.set("v.resultsOpen", false);
+                helper.loadQuestions(component, event, quizId, attempt);
+                break;
+            case "RESULTS":
+                component.set("v.registerOpen", false);
+                component.set("v.quizOpen", false);
+                component.set("v.resultsOpen", true);
+                component.set("v.results", results);
+                break;
+            default:
+                component.set("v.registerOpen", true);
+                component.set("v.quizOpen", false);
+                component.set("v.resultsOpen", false);
+        }
+    }
+})
